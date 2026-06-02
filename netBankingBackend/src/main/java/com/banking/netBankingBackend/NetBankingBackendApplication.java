fe31788ac01bf31961196be49f222449caa8d86e
Kartikeya <kartikeyayash4@gmail.com>
2026-06-02 23:15:31 +0530

    Connected to the DB removed Spring Security for a while

--- netBankingBackend/src/main/java/com/banking/netBankingBackend/NetBankingBackendApplication.java
+++ netBankingBackend/src/main/java/com/banking/netBankingBackend/NetBankingBackendApplication.java
@@ -2,9 +2,8 @@ package com.banking.netBankingBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class NetBankingBackendApplication {
 
 	public static void main(String[] args) {
